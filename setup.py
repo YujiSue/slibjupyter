449a53cc5d0fb67eb170d68ca8270427174ed9a4
Yuji Suehiro <yuji.sue@gmail.com>
2021-01-25 22:30:15 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SAppRun',
+    version='1.0.0',
+    author='Yuji Suehiro',
+    author_email='yuji.sue@gmail.com',
+    py_modules=['sapprun'],
+    url='https://github.com/YujiSue/slibjupyter',
+    license='MIT license',
+    description='Plugin to run C++ with slib on the Google Colab'
+)
