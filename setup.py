2125abdaaf6b050eb1ddb4dd8192a70f623d6c04
Yuji Suehiro <yuji.sue@gmail.com>
2021-01-26 09:00:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SAppRun',
-    version='1.0.0',
+    version='0.1.0',
     author='Yuji Suehiro',
     author_email='yuji.sue@gmail.com',
     py_modules=['slib_plugin', 'sapprun'],
