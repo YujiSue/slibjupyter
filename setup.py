49f5d2b231bd77822319b4ac263167776221ac4f
YujiSue <yuji.sue@gmail.com>
2021-03-30 14:10:07 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Yuji Suehiro',
     author_email='yuji.sue@gmail.com',
-    py_modules=['slib_plugin', 'sscriptrun', 'scoderun'],
+    py_modules=['slib_plugin', 'slibrun'],
     url='https://github.com/YujiSue/slibjupyter',
     license='MIT license',
     description='Plugin to run C++ with slib on the Google Colab'
