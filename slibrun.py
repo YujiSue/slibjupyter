9c01bd047fa19b1e23bfa83a3fbe71328d789378
YujiSue <yuji.sue@gmail.com>
2021-03-30 14:17:41 +0900

    update

--- slibrun.py
+++ slibrun.py
@@ -28,35 +28,35 @@ class SLibCodeRun(Magics):
             f.write(cell)
             f.write(footer)
 
-	def exportSrc(self, name, cell):
+    def exportSrc(self, name, cell):
         path = './Codes/'+name+'.cpp'
         with open(path, mode='w') as f:
             f.write(cell)
-            
+
     def compile(self, info):
         cmd = 'g++ -std=c++11 -O2'# -I./Codes -I/usr/local/include/slib -L/usr/local/lib -o ./App/' + info["product"]
         cmd += ' -I./Codes -I/usr/local/include/slib'
-		if(info['includes']):
-			for head in info['includes']:
-				cmd += ' -I'+head
-		if(info['codes']):
-			for code in info['codes']:
-				cmd += ' ./Codes/'+code
-		if(info['libs']):
-			for lib in info['libs']:
-				cmd += ' -l'+lib
-		cmd += ' -o ./App/' + info["product"]
-		if (info["verbose"]):
-			print(cmd)
+        if(info['includes']):
+            for head in info['includes']:
+                cmd += ' -I'+head
+        if(info['codes']):
+            for code in info['codes']:
+                cmd += ' ./Codes/'+code
+        if(info['libs']):
+            for lib in info['libs']:
+                cmd += ' -l'+lib
+        cmd += ' -o ./App/' + info["product"]
+        if (info["verbose"]):
+            print(cmd)
         proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if (info["verbose"]):
-			print(proc.stdout.splitlines())
+            print(proc.stdout.splitlines())
         
     def runScript(self, name, slibs, cell):
         self.preset()
         self.exportScript(name, slibs, cell)
-		libs = ['sobj', 'curl']
-		#
+        libs = ['sobj', 'curl']
+        #
         self.compile({'product':name, 'libs':libs, 'codes':['./Codes/'+name+'.cpp'], 'verbose': True})
         proc = subprocess.run('./App/'+name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         res = proc.stdout.splitlines()
@@ -64,19 +64,19 @@ class SLibCodeRun(Magics):
             print(row.decode())
         return None
 
-	def runCodes(self, name, codes, headers, libs, cell):
-		self.preset()
-		self.exportSrc(name, cell)
-		self.compile({'product':name,'includes':headers,'libs':libs,'codes':codes})
-		proc = subprocess.run('./App/'+name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    def runCodes(self, name, codes, headers, libs, cell):
+        self.preset()
+        self.exportSrc(name, cell)
+        self.compile({'product':name,'includes':headers,'libs':libs,'codes':codes})
+        proc = subprocess.run('./App/'+name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         res = proc.stdout.splitlines()
         for row in res:
             print(row.decode())
         return None
 
-	@cell_magic
-	def scompile(self, line, cell):
-		args = line.split()
+    @cell_magic
+    def scompile(self, line, cell):
+        args = line.split()
 
 
     @cell_magic
@@ -86,23 +86,23 @@ class SLibCodeRun(Magics):
         libs = args[1]
         output = self.runScript(name, libs, cell)
         return output
-	
-	@cell_magic
+    
+    @cell_magic
     def scoderun(self, line, cell):
         args = line.split()
         name = args[0]
-		codes = args[1]
-		headers = args[2]
+        codes = args[1]
+        headers = args[2]
         libs = args[3]
         output = self.runCodes(name, codes, headers, libs, cell)
         return output
 
-	@cell_magic
+    @cell_magic
     def sapprun(self, line, cell):
         args = line.split()
         name = args[0]
-		codes = args[1]
-		headers = args[2]
+        codes = args[1]
+        headers = args[2]
         libs = args[3]
         output = self.runCodes(name, codes, headers, libs, cell)
         return output
