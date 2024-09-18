4c0608350b3528ddcc26a6e8b87112af05e38d70
Yuji Suehiro <yuji.sue@gmail.com>
2024-09-18 14:57:00 +0900

    Update slibplugin.py

--- slibplugin.py
+++ slibplugin.py
@@ -1,4 +1,4 @@
 from slibrun import SLibCodeRun as SRunner
 def load_ipython_extension(ip):
-	slibplugin = SRunner(ip)
-	ip.register_magics(slibplugin)
+    slibplugin = SRunner(ip)
+    ip.register_magics(slibplugin)
