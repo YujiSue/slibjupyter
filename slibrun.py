e706bfc3b7a3d789bd0403002504d53378be5184
Yuji Suehiro <yuji.sue@gmail.com>
2024-09-18 18:24:04 +0900

    Update slibrun.py

--- slibrun.py
+++ slibrun.py
@@ -26,7 +26,7 @@ class SLibCodeRun(Magics):
             if '_SFUNC_' in row:
                 code = code + 'struct {\n'
                 part = row[row.find('_SFUNC_')+7:]
-		ret = re.search(r'\s+[a-zA-Z0-9_]+\s+', part)
+                ret = re.search(r'\s+[a-zA-Z0-9_]+\s+', part)
                 code  = code + ret.group() + 'operator()'
                 fname = re.search(r'\s+[a-zA-Z0-9_]+\s*\(', part)
                 code = code + part[fname.end()-1:] + '\n'
