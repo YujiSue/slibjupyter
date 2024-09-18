c30faa5db669f0b4c3ac26c594ace0e007e8ba36
Yuji Suehiro <yuji.sue@gmail.com>
2024-09-18 14:58:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
-
 setup(
-	name='slibjupyter',
-	version='0.1.0',
-	author='Yuji Suehiro',
-	py_modules=['slibplugin', 'slibrun'],
-	url='https://github.com/YujiSue/slibjupyter',
-	license='MIT license',
-	description='Plugin to run C++ with slib on the Google Colab'
+    name='slibjupyter',
+    version='0.1.0',
+    author='Yuji Suehiro',
+    py_modules=['slibplugin', 'slibrun'],
+    url='https://github.com/YujiSue/slibjupyter',
+    license='MIT license',
+    description='Plugin to run C++ with slib on Jupyter notebook'
 )
