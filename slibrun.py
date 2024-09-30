509d6dff3848555d62c81018c11f5c4e169b8236
YujiSue <yuji.sue@gmail.com>
2024-09-30 15:09:21 +0900

    update

--- slibrun.py
+++ slibrun.py
@@ -8,6 +8,7 @@ from IPython.core.magic import Magics, cell_magic, magics_class
 class SLibCodeRun(Magics):
     def __init__(self, shell):
         super(SLibCodeRun, self).__init__(shell)
+        self.preset()
 
     @staticmethod
     def preset():
@@ -25,79 +26,68 @@ class SLibCodeRun(Magics):
         rows = cell.splitlines()
         for row in rows:
             if '_DEF_' in row:
-                code = code + 'struct {\n'
+                code += 'struct {\n'
                 part = row[row.find('_DEF_')+5:]
                 ret = re.search(r'\s+[a-zA-Z0-9_]+\s+', part)
-                code = code + ret.group() + 'operator()'
+                code += ret.group() + 'operator()'
                 fname = re.search(r'\s+[a-zA-Z0-9_]+\s*\(', part)
-                code = code + part[fname.end()-1:] + '\n'
+                code += f'{part[fname.end()-1:]}\n'
                 name = part[fname.start():fname.end()-1]
                 brackets = brackets + part.count('{') - part.count('}')
                 if brackets == 0:
-                    code = code + '}' + name + ';'
+                    code += f'}}{name};'
                 else:
                     isFunc = True
             elif isFunc:
-                code = code + row + '\n'
+                code += f'{row}\n'
                 brackets = brackets + row.count('{') - row.count('}')
                 if brackets == 0:
-                    code = code + '}' + name + ';'
+                    code += f'}}{name};'
                     isFunc = False
             else:
-                code = code + row + '\n'
+                code += f'{row}\n'
         return code
-        
-    def makeScriptBody(self):
-        body = ''
-        scripts = glob.glob("./Scripts/*.cpscrpt")
-        sorted(scripts, key=lambda f: os.stat(f).st_mtime, reverse=True)
-        for scrpt in scripts:
-            with open(scrpt) as f:
-                body += f"{f.read()}\n"
-        return body
-    
-    def exportScript(self, name, libs, cell):
-        code = self.convertFunc(cell)
-        if name[0] == '+':
-            name = name[1:]
-            path = './Scripts/'+name+'.cpscrpt'
-            with open(path, mode='w') as s:
-                s.write(code)
-            body = self.makeScriptBody()
-        else:
-            body = self.makeScriptBody() + code		
-        
-        header = '#include "sobj.h"\n'
-        if 'A' in libs:
-            header += '#include "sapp.h"\n'
-        if 'I' in libs:
-            header += '#include "sbioinfo.h"\n'
-        header += 'using namespace slib;\n'
-        header += 'int main(int argc, const char **argv) {\n'
-        footer = '\nreturn 0;\n}'
-        path = './Codes/'+name+'.cpp'
-        with open(path, 'w') as f:
-            f.write(header)
-            f.write(body)
-            f.write(footer)
 
-    def exportSrc(self, name, cell):
-        path = f"./Codes/{name}.cpp"
+    def makeCode(self, name, headers, parts, code):
+        hpart = '#include "sobj.h"\n'
+        body = 'int main(int argc, const char **argv) {\n'
+        #
+        for header in headers:
+            hpart += f'#include {header}\n'
+        hpart += 'using namespace slib;\n'
+        hpart += 'using namespace slib::sio;\n'
+        hpart += 'using namespace slib::sutil;\n'
+        hpart += 'using namespace slib::smath;\n'
+        #
+        sorted(parts, key=lambda f: os.stat(f).st_mtime, reverse=True)
+        for script in parts:
+            if script.endswith(f"{name}.cpart"):
+                continue
+            with open(script) as fp:
+                body += f"{fp.read()}\n"
+        #
+        body += f"{code}\nreturn 0;\n}}"
+        #
+        path = os.path.join('Codes', f"{name}.cpp")
         with open(path, 'w') as f:
-            f.write(cell)
+            f.write(hpart)
+            f.write(body)
 
     def compile(self, info):
-        cmd = 'g++ -std=c++11 -O2'
-        cmd += ' -I./Codes -I/usr/local/include/slib'
+        # GCC
+        cmd = 'g++ -std=c++11 -O2 -I./Codes'
+        # 
+        cmd += ' -I/usr/local/include/slib'
+        # 
         if 'includes' in info:
             for head in info['includes']:
-                cmd += ' -I'+head
+                cmd += f' -I{head}'
         if 'codes' in info:
             for code in info['codes']:
-                cmd += ' ./Codes/'+code
+                cmd += f' {code}'
         if 'libs' in info:
             for lib in info['libs']:
-                cmd += ' -l'+lib
+                cmd += f' -{lib}'
         cmd += f" -o ./App/{info['product']}"
         if info['verbose']:
             print('*' * 30, ' Compile log ', '*' * 30, '\n >', cmd, '\n')
@@ -110,18 +100,11 @@ class SLibCodeRun(Magics):
         else:
             os.system(cmd)
 
-    def runScript(self, name, slibs, verbose, cell):
-        self.preset()
-        self.exportScript(name, slibs, cell)
-        libs = ['sobj', 'curl']
-        if 'A' in slibs:
-            libs.append('sapp')
-        if 'I' in slibs:
-            libs.append('sbioinfo')
-        if name[0] == '+':
-            name = name[1:]
-        self.compile({'product':name, 'libs':libs, 'codes':[name+'.cpp'], 'verbose': verbose})
-        proc = subprocess.Popen('./App/'+name, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, text=True)
+    def runScript(self, name, includes, headers, libs, parts, code, verbose):
+        self.savecscript(name, code)
+        self.makeCode(name, headers, parts, code)
+        self.compile({'product':name, 'includes':includes, 'libs':libs, 'codes':[os.path.join('Codes', f"{name}.cpp")], 'verbose': verbose})
+        proc = subprocess.Popen(os.path.join('App', name), shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, text=True)
         while proc.poll() is None:
             while True: 
                 line = proc.stdout.readline()
@@ -135,6 +118,7 @@ class SLibCodeRun(Magics):
                 print(line, end='')
             else:
                 break
+
     def runCodes(self, name, codes, headers, libs, cell):
         self.preset()
         self.exportSrc(name, cell)
@@ -144,43 +128,49 @@ class SLibCodeRun(Magics):
             line = proc.stdout.readline().strip()
             if line:
                 print(line)
-    @cell_magic
-    def savecode(self, line, cell):
-        args = line.split()
-        name = args[0]
-        self.exportSrc(name, cell)
+    
+    def savecscript(self, name, code):
+        codepath = os.path.join(os.getcwd(), "Scripts", f"{name}.cpart")
+        with open(codepath, 'w') as f:
+            f.write(code)
         
     @cell_magic
     def slibscript(self, line, cell):
         args = line.split()
         name = args[0]
-        libs = ''
+        libs = ['lsobj', 'lpthread', 'ldl', 'lcurl']
+        includes = []
+        headers = []
+        parts = []
         verbose = False
-        if 1 < len(args):
-            for i in range(1,  len(args)):
-                if args[i] == 'v':
-                    verbose = True
-                else :
-                    libs += args[i]
-        output = self.runScript(name, libs, verbose, cell)
-        return output
+        code = self.convertFunc(cell)
+        for arg in args[1:]:
+            if arg[0] == '+':
+                parts.append(os.path.join("Scripts", f'{arg[1:]}.cpart'))
+            elif ':' in arg:
+                if arg.startswith('l:'):
+                    libs.append(arg.replace(':', ''))
+                elif arg.startswith('h:'):
+                    headers.append(f'"{arg[2:]}.h"')
+                elif arg.startswith('H:'):
+                    includes.append(f'{arg[2:]}')
+            else:
+                for opt in arg:
+                    if opt == 'v':
+                        verbose = True
+        output = self.runScript(name, includes, headers, libs, parts, code, verbose)
+        return output    
     
     @cell_magic
-    def slibcodes(self, line, cell):
+    def slibcode(self, line, cell):
         args = line.split()
         name = args[0]
         codes = args[1]
         headers = args[2]
         libs = args[3]
-        output = self.runCodes(name, codes, headers, libs, cell)
+        output = self.runCode(name, codes, headers, libs, cell)
         return output
     
     @cell_magic
-    def sappexec(self, line, cell):
-        args = line.split()
-        name = args[0]
-        codes = args[1]
-        headers = args[2]
-        libs = args[3]
-        output = self.runCodes(name, codes, headers, libs, cell)
-        return output
+    def slibapp(self, line, cell):
+        return ''
